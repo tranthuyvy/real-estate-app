5cd781a6637f06db738012494c05c5ef19634fe4
Vy Tran <ttv.thuyvy.1544@gmail.com>
2025-02-13 10:34:17 +0700

    setup project

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// ------------------------------------------
+
+const Explore = () => {
+  return (
+    <View>
+      <Text>Explore</Text>
+    </View>
+  );
+};
+
+export default Explore;
