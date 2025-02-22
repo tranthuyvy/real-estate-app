26a6d79cf3c04924f3a32caa6a93c12d559516a6
Vy Tran <ttv.thuyvy.1544@gmail.com>
2025-02-22 16:42:43 +0700

    search bar

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,30 @@
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Image, SafeAreaView, Text, View } from 'react-native';
+
+import images from '@/constants/images';
+import icons from '@/constants/icons';
+
+// ------------------------------------------------------------
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text className="font-bold text-3xl font-rubik my-10 text-primary-300">
-        Welcome to RealEstate
-      </Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                TTV
+              </Text>
+            </View>
+          </View>
+
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ components/search.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { useLocalSearchParams, usePathname, router } from 'expo-router';
+import {
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { useDebouncedCallback } from 'use-debounce';
+
+import icons from '@/constants/icons';
+
+// ------------------------------------------------------------
+
+export default function Search() {
+  const path = usePathname();
+  const params = useLocalSearchParams<{ query?: string }>();
+  const [seach, setSearch] = useState(params.query);
+
+  const debouncedSearch = useDebouncedCallback((text: string) => {
+    router.setParams({ query: text });
+  }, 500);
+
+  const handleSearch = (text: string) => {
+    setSearch(text);
+    debouncedSearch(text);
+  };
+
+  return (
+    <View className="flex flex-row items-center justify-between w-full px-4 rounded-full bg-accent-100 border border-primary-100 mt-5 py-2">
+      <View className="flex-1 flex-row items-center justify-start z-50">
+        <Image source={icons.search} className="size-5" />
+        <TextInput
+          value={seach}
+          onChangeText={handleSearch}
+          placeholder="Search..."
+          className="text-sm font-rubik text-black-300 ml-2 flex-1"
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Image source={icons.filter} className="size-5" />
+      </TouchableOpacity>
+    </View>
+  );
+}
